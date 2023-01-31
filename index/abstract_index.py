7e38b9eeb8a9d174b2ab3639c64b8b153c01aece
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-01-31 22:32:11 +0100

    Replace index by abstract class

--- index/abstract_index.py
+++ index/abstract_index.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 import re
 import requests
+from abc import ABC, abstractmethod
 
-
-class Index:
+class AbstractIndex(ABC):
     def __init__(self, documents=None) -> None:
         self.index = dict()
         self.documents = documents
@@ -31,40 +31,10 @@ class Index:
         # lowercase, remove leading and trailing whitespace and split the string
         return text.lower().strip().split()
 
-    def create_inverted_index(self, doc_id, tokens):
-        appeared_words = dict()
-        # compute the frequency by token
-        for token in tokens:
-            token_frequency = appeared_words[token] if token in appeared_words else 0
-            appeared_words[token] = token_frequency + 1
-        # print(appeared_words, '\n\n\n')
-
-        # update the inverted index for each token
-        update_dict = {
-            key: appearance
-            if key not in self.index
-            else self.index[key] + [appearance]
-            for (key, appearance) in appeared_words.items()
-        }
-        # sort the tokens by appearance
-        update_dict = sorted(update_dict.items(), key=lambda kv: kv[1], reverse=True)
-        # update the index
-        self.index.update(update_dict)
+    @abstractmethod
+    def create_index(self, doc_id, tokens):
+        pass
 
     def metadata(self):
         pass
 
-
-if __name__ == "__main__":
-    import json
-    with open("crawled_urls.json") as json_file:
-        json_data = json.loads(json_file.read())
-
-    link = json_data[8]
-    # print(link)
-    index = Index()
-    raw_text = index.get_text_from_url(link)
-    tokens = index.tokenize(raw_text)
-    # print(raw_text)
-    index.create_inverted_index(doc_id=1, tokens=tokens)
-    print(index.index)
