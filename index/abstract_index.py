b54448e420087756d8d9636d8f8b8d1334724b14
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-01 17:16:50 +0100

    Abstract index

--- index/abstract_index.py
+++ index/abstract_index.py
@@ -1,26 +1,46 @@
 from abc import ABC, abstractmethod
 from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+import logging
 import re
 import requests
 import string
 
 
 class AbstractIndex(ABC):
-    def __init__(self, urls=None) -> None:
-        self.index = dict()
+    def __init__(self, index={}, urls=None, delete_stopwords=False) -> None:
+        self.index = index
         self.urls = urls
-        self.visited_urls = {}
+        self.tokens = []
+        self.nb_tokens = 0
+        self.nb_documents = len(self.urls)
+        self.visited_urls = []
+        self.nb_visited_urls = 0
+        self.failed_urls = []
+        self.nb_failed_urls = 0
+        if delete_stopwords == True:
+            self.stopwords = stopwords.words('french')
+        else:
+            self.stopwords = None
 
     def download_url(self, url):
-        return requests.get(url).text
+        try:
+            req = requests.get(url)
+            if req.status_code == 200:
+                return req.text
+            else:
+                return None
+        except Exception:
+            logging.warning(f'Failed to reach {url}')
 
     def get_text_from_url(self, url):
         html = self.download_url(url=url)
-        soup = BeautifulSoup(html, 'html.parser')
-        raw_text = ''
-        for paragraph in soup.find_all('p'):
-            raw_text += " " + paragraph.text
-        return raw_text
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            raw_text = ''
+            for paragraph in soup.find_all([f'h{i}' for i in range(1, 7)]):
+                raw_text += " " + paragraph.text
+            return raw_text
 
     def tokenize(self, text):
         """
@@ -33,11 +53,35 @@ class AbstractIndex(ABC):
             list: list of tokens
         """
         text = re.sub(r"(?i)^(?:(?![×Þß÷þø])[-'0-9a-zÀ-ÿ])+$", "", text)
-        return ''.join(' ' if char in string.punctuation else char for char in text).lower().strip('').split(sep=" ")
+        text = re.sub(r"’|…", " ", text)
+        tokens = ''.join(
+            ' ' if char in string.punctuation else char for char in text).lower().strip('').split()
+        # compute some statistics
+        for token in tokens:
+            if token not in self.tokens:
+                self.tokens.append(token)
+                self.nb_tokens += 1
+        # delete stopwords
+        if self.stopwords:
+            tokens = [token for token in tokens if not token.lower()
+                      in self.stopwords]
+        return tokens
 
     @abstractmethod
     def create_index(self, doc_id, tokens):
         pass
 
-    def metadata(self):
-        pass
+    def get_statistics(self):
+        return (
+            f"------ Statistics ------\n"
+            f"Number of documents: {self.nb_documents}\n"
+            f"Number of tokens: {self.nb_tokens}\n"
+            f"Mean number of tokens per document: {round(self.nb_tokens/self.nb_documents)}\n"
+        )
+
+    def get_metadata(self):
+        return (
+            f"------- Metadata -------\n"
+            f"Number of URLs visited: {self.nb_visited_urls}\n"
+            f"Number of failed URLs: {self.nb_failed_urls}\n"
+        )
