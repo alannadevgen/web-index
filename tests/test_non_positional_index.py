5fdaae82e6b9103d7904049fede5c3a1257adad9
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-02 22:44:08 +0100

    Test stemmer and stopwords

--- tests/test_non_positional_index.py
+++ tests/test_non_positional_index.py
@@ -20,6 +20,22 @@ class TestPositionalIndex(TestCase):
         self.assertIsNotNone(result2)
         self.assertIn('Design Patterns', result2)
 
+
+    def test_get_html_fail(self):
+        # GIVEN
+        url1 = "https://www.degriffelectromenager.com/"
+        url2 = "http://www.amisducadrenoir.fr/"
+        # WHEN
+        non_positional_index = NonPositionalIndex(urls=[url1, url2])
+        result1 = non_positional_index.get_text_from_url(url=url1)
+        result2 = non_positional_index.get_text_from_url(url=url2)
+        # THEN
+        self.assertIsInstance(non_positional_index, NonPositionalIndex)
+        self.assertIsNone(result1)
+        self.assertIsNone(result2)
+        self.assertRaises(Exception, result1)
+        self.assertRaises(Exception, result2)
+
     def test_get_html_header(self):
         # GIVEN
         url1 = "https://asana.com/resources/best-morning-routine"
@@ -46,9 +62,8 @@ class TestPositionalIndex(TestCase):
         # THEN
         self.assertIsInstance(non_positional_index, NonPositionalIndex)
         self.assertIsNotNone(index)
-        # self.assertIn('Design Patterns', result1)
-        self.assertEqual(index['routine'][0], {"doc_id": 0, "count": 28})
-        self.assertEqual(index['design'][0], {"doc_id": 1, "count": 6})
+        self.assertEqual(index['schedule'][0], {"doc_id": 0, "count": 1})
+        self.assertEqual(index['enter'][0], {"doc_id": 1, "count": 1})
 
     def test_run_index(self):
         # GIVEN
@@ -62,4 +77,53 @@ class TestPositionalIndex(TestCase):
         self.assertIsInstance(non_positional_index, NonPositionalIndex)
         self.assertIsNotNone(index)
         self.assertEqual(index['rate'][0], {'doc_id': 0, 'count': 1})
-        self.assertEqual(index['design'][0], {'doc_id': 1, 'count': 6}) 
+        self.assertEqual(index['affirmations'][0], {'doc_id': 0, 'count': 7}) 
+    
+
+    def test_export_metadata(self):
+        # GIVEN
+        url1 = "https://asana.com/resources/best-morning-routine"
+        url2 = "https://refactoring.guru/"
+        # WHEN
+        non_positional_index = NonPositionalIndex(urls=[url1, url2], type="title")
+        non_positional_index.run()
+        metadata = non_positional_index.export_metadata()
+        # THEN
+        self.assertIsInstance(non_positional_index, NonPositionalIndex)
+        self.assertIsNotNone(metadata)
+        self.assertEqual(metadata['nb_documents'], 2)
+        self.assertEqual(metadata['nb_visited_urls'], 2)
+        self.assertEqual(metadata['nb_failed_urls'], 0)
+        self.assertEqual(metadata['nb_total_urls'], 2)
+
+    def test_stopwords(self):
+        # GIVEN
+        url1 = "https://fr.wikipedia.org/wiki/Jeux_olympiques"
+        url2 = "https://fr.wikipedia.org/wiki/Lotus_pedunculatus"
+        # WHEN
+        non_positional_index = NonPositionalIndex(urls=[url1, url2], type="paragraph", delete_stopwords=False)
+        non_positional_index.run()
+        index = non_positional_index.get_index()
+        # THEN
+        self.assertIsInstance(non_positional_index, NonPositionalIndex)
+        self.assertIsNotNone(index)
+        self.assertIn('ce', index.keys())
+        self.assertIn('le', index.keys())
+        self.assertIn('ceux', index.keys())
+
+    
+    def test_stemmer(self):
+        # GIVEN
+        url1 = "https://fr.wikipedia.org/wiki/Jeux_olympiques"
+        url2 = "https://fr.wikipedia.org/wiki/Lotus_pedunculatus"
+        # WHEN
+        non_positional_index = NonPositionalIndex(urls=[url1, url2], type="paragraph", use_stemmer=True)
+        non_positional_index.run()
+        index = non_positional_index.get_index()
+        # THEN
+        self.assertIsInstance(non_positional_index, NonPositionalIndex)
+        self.assertIsNotNone(index)
+        self.assertNotIn('olympique', index.keys())
+        self.assertNotIn('inflammatoire', index.keys())
+        self.assertIn('olymp', index.keys())
+        self.assertIn('inflammatoir', index.keys())
--- tests/test_positional_index.py
+++ tests/test_positional_index.py
@@ -35,17 +35,77 @@ class TestPositionalIndex(TestCase):
         self.assertIsNotNone(result2)
         self.assertIn('Design Patterns', result2)
 
-    def test_get_html_header(self):
+    def test_get_html_paragraph(self):
         # GIVEN
         url1 = "https://asana.com/resources/best-morning-routine"
         url2 = "https://refactoring.guru/"
         # WHEN
         positional_index = PositionalIndex(urls=[url1, url2], type="paragraph")
-        result1 = positional_index.get_text_from_url(url=url1)
-        result2 = positional_index.get_text_from_url(url=url2)
+        positional_index.run()
+        index = positional_index.get_index()
         # THEN
         self.assertIsInstance(positional_index, PositionalIndex)
-        self.assertIsNotNone(result1)
-        self.assertIn('healthy breakfast', result1)
-        self.assertIsNotNone(result2)
-        self.assertIn('Design Patterns', result2)
+        self.assertIsNotNone(index)
+        self.assertEqual(index['act'][0], {"doc_id": 0, "position": [1589], "count": 1})
+        self.assertEqual(index['design'][0], {"doc_id": 1, "position": [2, 16, 114, 181, 182, 193, 207], "count": 7})
+
+    def test_run_index(self):
+        # GIVEN
+        url1 = "https://asana.com/resources/best-morning-routine"
+        url2 = "https://refactoring.guru/"
+        # WHEN
+        positional_index = PositionalIndex(urls=[url1, url2], type="title")
+        positional_index.run()
+        index = positional_index.get_index()
+        # THEN
+        self.assertIsInstance(positional_index, PositionalIndex)
+        self.assertIsNotNone(index)
+        self.assertEqual(index['rate'][0], {'doc_id': 0, 'position': [1060], 'count': 1})
+        self.assertEqual(index['after'][0], {'doc_id': 0, 'position': [298, 402, 1440], 'count': 3}) 
+
+    def test_export_metadata(self):
+        # GIVEN
+        url1 = "https://asana.com/resources/best-morning-routine"
+        url2 = "https://refactoring.guru/"
+        # WHEN
+        positional_index = PositionalIndex(urls=[url1, url2], type="title")
+        positional_index.run()
+        metadata = positional_index.export_metadata()
+        # THEN
+        self.assertIsInstance(positional_index, PositionalIndex)
+        self.assertIsNotNone(metadata)
+        self.assertEqual(metadata['nb_documents'], 2)
+        self.assertEqual(metadata['nb_visited_urls'], 2)
+        self.assertEqual(metadata['nb_failed_urls'], 0)
+        self.assertEqual(metadata['nb_total_urls'], 2)
+
+    def test_stopwords(self):
+        # GIVEN
+        url1 = "https://fr.wikipedia.org/wiki/Jeux_olympiques"
+        url2 = "https://fr.wikipedia.org/wiki/Lotus_pedunculatus"
+        # WHEN
+        positional_index = PositionalIndex(urls=[url1, url2], type="paragraph", delete_stopwords=True)
+        positional_index.run()
+        index = positional_index.get_index()
+        # THEN
+        self.assertIsInstance(positional_index, PositionalIndex)
+        self.assertIsNotNone(index)
+        self.assertIn('la', index.keys())
+        self.assertIn('le', index.keys())
+        self.assertNotIn('les', index.keys())
+
+    def test_stemmer(self):
+        # GIVEN
+        url1 = "https://fr.wikipedia.org/wiki/Jeux_olympiques"
+        url2 = "https://fr.wikipedia.org/wiki/Lotus_pedunculatus"
+        # WHEN
+        positional_index = PositionalIndex(urls=[url1, url2], type="paragraph", use_stemmer=True)
+        positional_index.run()
+        index = positional_index.get_index()
+        # THEN
+        self.assertIsInstance(positional_index, PositionalIndex)
+        self.assertIsNotNone(index)
+        self.assertNotIn('olympique', index.keys())
+        self.assertNotIn('inflammatoire', index.keys())
+        self.assertIn('olymp', index.keys())
+        self.assertIn('inflammatoir', index.keys())
