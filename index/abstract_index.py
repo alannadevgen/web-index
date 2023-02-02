da2a6c1e4c486b281e8cc863f2d0f56a0d22178c
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-02 22:57:16 +0100

    Abstract index documentation

--- index/abstract_index.py
+++ index/abstract_index.py
@@ -1,13 +1,13 @@
+import string
+import requests
+import re
+import logging
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
 from abc import ABC, abstractmethod
 from bs4 import BeautifulSoup
 import nltk
 nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-import logging
-import re
-import requests
-import string
 
 TYPES = {
     "title": 'title',
@@ -39,11 +39,25 @@ class AbstractIndex(ABC):
         self.type = type
         self.print_logs = print_logs
         self.print_metadata = print_metadata
-        self.stopwords = stopwords.words('french') if delete_stopwords else None
+        self.stopwords = stopwords.words(
+            'french') if delete_stopwords else None
         self.use_stemmer = use_stemmer
         self.stemmer = SnowballStemmer('french') if use_stemmer else None
 
     def __download_url(self, url):
+        '''
+        Gets the text of the web page for a given URL
+
+        Parameters
+        ----------
+        url:str
+            URL from which download the HTML text page
+
+        Returns
+        -------
+        str
+            text contained in the web page
+        '''
         try:
             req = requests.get(url)
             if req.status_code == 200:
@@ -57,6 +71,20 @@ class AbstractIndex(ABC):
                 logging.warning(f'Failed to reach {url}')
 
     def get_text_from_url(self, url):
+        '''
+        Gets the text of the web page for a given URL
+
+        Parameters
+        ----------
+        url:str
+            URL from which we fetch the info
+
+        Returns
+        -------
+        str
+            titles, headers or paragraphs depending on the type of user query 
+            as raw string (all glued together)
+        '''
         html = self.__download_url(url=url)
         if html:
             soup = BeautifulSoup(html, 'html.parser')
@@ -70,6 +98,7 @@ class AbstractIndex(ABC):
         In order to tokenize the text we need to:
             - Delete all strings that are not letters or numbers and punctuation in the string
             - Lowercase, remove leading and trailing whitespace and split the string
+        We can also delete (French) stopwords and use to stem the tokens
 
         Returns
         -------
@@ -98,13 +127,22 @@ class AbstractIndex(ABC):
         return tokens
 
     @abstractmethod
-    def create_index(self, doc_id, tokens):
+    def create_index(self):
+        '''
+        Build the index by browsing the tokens
+        '''
         pass
 
     def get_index(self):
+        '''
+        Getter for the index
+        '''
         return self.__index
 
     def get_statistics(self):
+        '''
+        Print some statistics such as the number of documents or the number of (distinct) tokens
+        '''
         return (
             f"------ Statistics ------\n"
             f"Number of documents: {self.nb_documents}\n"
@@ -113,6 +151,9 @@ class AbstractIndex(ABC):
         )
 
     def get_metadata(self):
+        '''
+        Print some statistics such as the number of URLs visited or that have failed to download
+        '''
         return (
             f"------- Metadata -------\n"
             f"Number of URLs visited: {self.nb_visited_urls}\n"
@@ -121,6 +162,14 @@ class AbstractIndex(ABC):
         )
 
     def export_metadata(self):
+        '''
+        Export the metadata
+
+        Returns
+        -------
+        dict
+            Dictionary containing all the metadata
+        '''
         metadata = {
             "nb_documents": self.nb_documents,
             "nb_tokens": self.nb_tokens,
@@ -130,7 +179,10 @@ class AbstractIndex(ABC):
             "nb_total_urls": self.nb_visited_urls + self.nb_failed_urls
         }
         return metadata
-    
+
     @abstractmethod
     def run(self):
+        '''
+        Process the indexing
+        '''
         pass
